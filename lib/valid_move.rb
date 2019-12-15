9efae46ffb262521174cd069ff83ba420f0361a9
Michael Diaz <mike.diaz006@gmail.com>
2019-12-15 14:33:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,13 @@
 # code your #valid_move? method here
-
-
+def valid_move?(array, index)
+  if position_taken(array, index) == true || index.between?
+  end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array, index)
+  result = nil
+  if array[index] == "X" || array[index] == "O"
+  result  = true
+  else result = false
+  end
+end
